ee79a26c2514b85fea56309b285d3863c7e0f187
Newton Jorge <75003439+cobeliii@users.noreply.github.com>
2024-01-14 10:02:01 -0600

    "Added third exercise on loops and arrays"

--- src/loopsandarrays/Exercise1.java
+++ src/loopsandarrays/Exercise1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        //Exercise 1 and 2
+        //Exercise 1, 2 3
 
         // declare an empty integer array of length = 3
         // print the contents to the console
@@ -16,6 +16,13 @@ public class Exercise1 {
         // fill it with the number `4`
         // print the array to the console
         Arrays.fill(intArray, 4);
+        System.out.println(Arrays.toString(intArray));
+
+        //Exercise 3
+        // reassign the second value in the array to the number `17`
+        // print the array to the console
+
+        intArray[1] = 17;
         System.out.println(Arrays.toString(intArray));
     }
 }

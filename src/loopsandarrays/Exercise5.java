678cd794cab58a7ba05f6198cb3e3cb5f1b7e80f
Newton Jorge <75003439+cobeliii@users.noreply.github.com>
2024-01-14 10:05:36 -0600

    "Exercise 5 has been added"

---
+++ src/loopsandarrays/Exercise5.java
@@ -0,0 +1,13 @@
+package loopsandarrays;
+
+import java.util.Arrays;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        // declare and initialize a String array which holds the values "a", "b", "c" and "d"
+        // print the array to the console
+
+        String[] stringArray = {"a", "b", "c", "d"};
+        System.out.println(Arrays.toString(stringArray));
+    }
+}

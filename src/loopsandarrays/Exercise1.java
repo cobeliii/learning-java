3f7137ba0abc3cb575beb125d3f8dd083a2e87b5
Newton Jorge <75003439+cobeliii@users.noreply.github.com>
2024-01-14 01:25:19 -0600

    "Added first exercise on loops and arrays"

---
+++ src/loopsandarrays/Exercise1.java
@@ -0,0 +1,13 @@
+package loopsandarrays;
+
+import java.util.Arrays;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        // declare an empty integer array of length = 3
+        // print the contents to the console
+
+        int[] intArray = new int[3];
+        System.out.println(Arrays.toString(intArray));
+    }
+}
